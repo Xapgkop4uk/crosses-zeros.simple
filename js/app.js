8e53a19019221d8b564ba5579ba3bcc99e013503
Vladimir Popinov <Vladimir.popinov@gmail.com>
2017-11-09 17:59:17 +0300

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,7 @@
+var APP_ID = 'F4938450-8412-F432-FF30-7FF933EE1300';
+var API_KEY = '0F451544-AEB6-6493-FFEA-731A9DDDA700';
+
+Backendless.serverURL = 'https://api.backendless.com';
+Backendless.initApp(APP_ID, API_KEY);
+
+                    
